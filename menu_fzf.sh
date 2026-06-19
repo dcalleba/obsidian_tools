52504f76bc815df9016e9f081dbe73208e70e4b7
dcalleba <daniel@callebaut.org>
2026-06-19 17:08:50 +0200

    Mono fichier de commande avec aide dans obsi_cockpit

--- menu_fzf.sh
+++ menu_fzf.sh
@@ -1,38 +1,100 @@
-#!/usr/bin/env bash
+#!/bin/zsh
 
-set -u
+FILE=~/Pythons/Obsidian_tools/commands.txt
+VAULT=~/OBSI_COCKPIT
+DOC_DIR="$VAULT/01 Comment Faire"
 
-PROJECT_DIR="$HOME/Pythons/Obsidian_tools"
-TITLE="${1:-MENU}"
-MEMO_FILE="${2:-}"
+CAT="$1"
 
-if [ -z "$MEMO_FILE" ]; then
-  echo "Usage: $0 TITRE fichier_memo.txt"
-  exit 1
-fi
+while true
+do
+    clear
+    echo "===== $CAT ====="
+    #echo
+    #echo "Numéro + Entrée : exécution directe"
+    #echo "Entrée seule    : menu avec aide"
+    #echo
 
-cd "$PROJECT_DIR" || exit 1
+    #read "?Commande n° : " direct
 
-while true; do
-  clear
-  echo "=== $TITLE ==="
+    #########################################
+    # Exécution directe par numéro
+    #########################################
 
-  cmd=$(
-    grep -v '^#' "$MEMO_FILE" |
-    grep -v '^$' |
-    fzf --no-sort --layout=reverse |
-    cut -d'|' -f2-
-  )
+    if [ -n "$direct" ]; then
 
-  [ -z "$cmd" ] && break
+        line=$(grep "^$direct|" "$FILE")
 
-  echo
-  echo "▶ $cmd"
-  echo
+        if [ -z "$line" ]; then
+            echo
+            echo "Commande inconnue : $direct"
+            echo
+            read "?Entrée..."
+            continue
+        fi
 
-  eval "$cmd"
+    #########################################
+    # Menu FZF avec aperçu de l'aide
+    #########################################
 
-  echo
-  echo "Appuyez sur Entrée..."
-  read -r
+    else
+
+        sel=$(
+            awk -F'|' -v cat="$CAT" '
+                $5==cat {
+                    print $1 "|" $2 "|" $4
+                }
+            ' "$FILE" |
+            fzf \
+                --no-sort \
+                --layout=reverse \
+                --delimiter='|' \
+                --with-nth=1,2 \
+                --preview='
+                    note=$(echo {} | awk -F"|" "{print \$3}" | sed "s/^ *//;s/ *$//")
+                    file="'"$DOC_DIR"'/${note}.md"
+
+                    if [ -f "$file" ]; then
+                        bat --style=plain --color=always "$file"
+                    else
+                        echo
+                        echo "Aide introuvable"
+                        echo
+                        echo "$file"
+                    fi
+                ' \
+                --preview-window=right:60%:wrap
+        )
+
+        [ -z "$sel" ] && break
+
+        num=$(echo "$sel" | cut -d'|' -f1 | xargs)
+
+        line=$(grep "^$num|" "$FILE")
+
+    fi
+
+    #########################################
+    # Extraction des champs
+    #########################################
+
+    description=$(echo "$line" | cut -d'|' -f2)
+    cmd=$(echo "$line" | cut -d'|' -f3)
+    note=$(echo "$line" | cut -d'|' -f4)
+
+    clear
+
+    echo "===== $CAT ====="
+    echo
+    echo "Description : $description"
+    echo "Commande    : $cmd"
+    echo "Documentation : $note.md"
+    echo
+    echo "▶ Exécution..."
+    echo
+
+    eval "$cmd"
+
+    echo
+    read "?Entrée..."
 done
